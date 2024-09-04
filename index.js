880017aeb15edb4ba5dc5e5d6618585bd0e99f35
dobby-zent <dobby.lee@zent.kr>
2024-09-04 20:57:16 +0900

    wip

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import express from 'express'
 
 const app = express()
 app.get("/",(req,res) => res.status(200).send("hello world"))
-app.get("/version", (req,res) => res.status(200).send(process.env.VERSION ?? "1.0.0"))
+app.get("/version", (req,res) => res.status(200).send(process.env.VERSION ?? "2.0.0"))
 app.listen(3000, () => console.log("Server is running on port 3000"))
