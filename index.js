c238096cf1b8d0035611b2d0b1a810147e480e6c
dobby-zent <dobby.lee@zent.kr>
2024-09-04 20:59:53 +0900

    wip

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import express from 'express'
 const app = express()
 app.get("/",(req,res) => res.status(200).send("hello world"))
 app.get("/version", (req,res) => res.status(200).send(process.env.VERSION ?? "2.0.0"))
+app.get("/hello",(req,res)=> res.status(200).jsone("my name is leedonggyu"))
 app.listen(3000, () => console.log("Server is running on port 3000"))
